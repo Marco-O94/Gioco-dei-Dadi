4209ca48659c37656fb9ea11886b23ae2449ba28
Marco Oliveri <marcooliveri94@gmail.com>
2022-02-15 15:54:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="it" dir="ltr">
 
 <head>
   <meta charset="utf-8">
-  <title>Dicee</title>
+  <title>Gioco dei dadi</title>
   <link rel="stylesheet" href="styles.css">
   <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Lobster" rel="stylesheet">
 </head>
